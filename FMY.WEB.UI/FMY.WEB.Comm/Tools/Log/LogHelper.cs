bb7bac9c61af8730752cbaea5ce26cc9c8d6b549
IamFMYS <1214211329@qq.com>
2017-05-03 22:35:56 +0800

    Log4net未完成

---
+++ FMY.WEB.UI/FMY.WEB.Comm/Tools/Log/LogHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FMY.WEB.Comm.Tools.Log
+{
+    public class LogTool
+    {
+        private readonly static object looker = new object();
+        private  LogTool logHelper;
+
+        //public LogTool LogHelper
+        //{
+        //    get { return logHelper; }
+        //    set { logHelper = value; }
+        //}
+
+        private LogTool()
+        { }
+
+    }
+}
