0bd748bd310e1405ba9b119efe0f98625482f9f7
T-fmy <Tfmy@outlook.com>
2017-11-02 17:40:27 +0800

    添加List控制器

--- FMY.WEB.UI/FMY.WEB.UI/Controllers/ListController.cs
+++ FMY.WEB.UI/FMY.WEB.UI/Controllers/ListController.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
 
 namespace FMY.WEB.UI.Controllers
 {
-    public class LoginController : Controller
+    public class ListController : Controller
     {
-        //
-        // GET: /Login/
-
         public ActionResult Index()
-        {            
-            //Response.StatusCode = 403;
-            throw new Exception("1");
+        {         
             return View();
         }
     }
