47ce4fba21c576ec2d2e795d0bd7093dca4f7bb3
IamFMYS <1214211329@qq.com>
2018-03-13 23:44:23 +0800

    WCF Practice

---
+++ FMY.WEB.UI/Host/CustomValidate/X509Validate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host.CustomValidate
+{
+    public class MyX509Validator : System.IdentityModel.Selectors.X509CertificateValidator
+    {
+
+        public override void Validate(System.Security.Cryptography.X509Certificates.X509Certificate2 certificate)
+        {
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("X509认证证书为空！");
+            }
+            if (certificate.Thumbprint != "3cebec23ab7c9604d5ea7309c469b10223ecd9be".ToUpper())
+            {
+                throw new System.IdentityModel.Tokens.SecurityTokenException("Certificate Validation Error!");
+            }
+        }
+    }
+}
