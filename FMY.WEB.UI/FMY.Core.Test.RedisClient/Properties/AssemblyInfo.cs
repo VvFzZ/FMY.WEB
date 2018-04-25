9ef3efd0a85c27e8d71f180c933c3ea9db29cd1e
T-fmy <Tfmy@outlook.com>
2018-04-25 17:46:23 +0800

    simple redis mongodb

---
+++ FMY.WEB.UI/FMY.Core.Test.RedisClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FMY.Core.Test.RedisClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FMY.Core.Test.RedisClient")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82691486-c53c-4962-a01b-542ea5278ad9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
