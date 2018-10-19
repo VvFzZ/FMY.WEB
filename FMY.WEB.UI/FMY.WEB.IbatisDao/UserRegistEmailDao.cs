a8538ad4b05edc0af32a9d6b57614df768620077
fmy <1214211329@qq.com>
2018-10-19 08:44:07 +0800

    IbatisUserRegistEamilDao修改

--- FMY.WEB.UI/FMY.WEB.IbatisDao/UserRegistEmailDao.cs
+++ FMY.WEB.UI/FMY.WEB.IbatisDao/UserRegistEmailDao.cs
@@ -11,9 +11,16 @@ namespace FMY.WEB.IbatisDao
 {
     class UserRegistEmailDao : IUserRegistEmailDao
     {
+
+        private string GetMapFullName(string statementName)
+        {
+            return string.Format("{0}.{1}.{2}", Mapper.MapperDomain, "UserRegistEmail", statementName);
+        }
+
         public int addEmailRecrd(UserRegistEmail model)
         {
-            throw new NotImplementedException();
+            string mapper = GetMapFullName("InsertUserRegistEmail");
+            return (int)Mapper.Instance.Insert(mapper, model);
         }
 
         public int GetIdByUidAndVcode(string userId, string validateCode)
