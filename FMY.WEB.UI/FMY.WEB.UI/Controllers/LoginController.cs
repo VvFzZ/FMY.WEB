2b562be2099c35c0e9fb1c191ea8c4aceeb422a1
fmy <1214211329@qq.com>
2018-10-19 08:46:09 +0800

    BubbleSort

--- FMY.WEB.UI/FMY.WEB.UI/Controllers/LoginController.cs
+++ FMY.WEB.UI/FMY.WEB.UI/Controllers/LoginController.cs
@@ -62,5 +62,30 @@ namespace FMY.WEB.UI.Controllers
 
             return Json(new Result(flag), JsonRequestBehavior.AllowGet);
         }
+
+        public void DoBubbleSort()
+        {
+            int[] arr = new int[] { 12,3,32,44,342,43};
+
+            int count = arr.Length - 1, lastChange, i = 0;
+
+            while (i<count)
+            {
+                lastChange = 0;
+
+                for (i = 0; i < count; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        arr[i] = arr[i] + arr[i + 1];
+                        arr[i + 1] = arr[i] - arr[i + 1];
+                        lastChange = i;
+                    }
+                }
+
+                i = 0;
+                count = lastChange;
+            }
+        }
     }
 }
