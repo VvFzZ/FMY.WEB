882c15e6d16f85b9370542c0a29cde661ee5bed0
fmy <1214211329@qq.com>
2018-10-19 09:03:01 +0800

    BubbleSort

---
+++ FMY.WEB.UI/FMY.WEB.Comm/Algorithm/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMY.WEB.Comm.Algorithm
+{
+    public class BubbleSort
+    {
+        public void DoBubbleSort1()
+        {
+            int[] arr = new int[] { 12, 3, 32, 44, 342, 43 };
+
+            int count = arr.Length - 1, lastChange, i = 0;
+
+            while (i < count)
+            {
+                lastChange = 0;
+
+                for (i = 0; i < count; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        arr[i] = arr[i] + arr[i + 1];
+                        arr[i + 1] = arr[i] - arr[i + 1];
+                        lastChange = i;
+                    }
+                }
+
+                i = 0;
+                count = lastChange;
+            }
+        }
+    }
+}
